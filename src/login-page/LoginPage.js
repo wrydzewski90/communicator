37e4390fe503cb331377c8d378476b22a6387b22
Wiktor Rydzewski <laplaya18@gmail.com>
2021-02-22 15:18:41 +0100

    Add right side

--- src/login-page/LoginPage.js
+++ src/login-page/LoginPage.js
@@ -1,14 +1,49 @@
 import "./loginPage.scss";
+import { useState } from "react";
 
 function LoginPage() {
-  return (
-    <div className="main">
-      <div className="leftSide">
-        <div className="image"></div>
-      </div>
-      <div className="rightSide"></div>
-    </div>
-  );
+    const [message, setMessage] = useState("login");
+    const firstclick = () => {
+        setMessage("login succes!");
+    };
+    return (
+        <div className="main">
+            <div className="leftSide">
+                <div className="image"></div>
+            </div>
+            <div className="rightSide">
+               <form action="/">
+                    <p className="login-title">member login</p>
+                    <div className="input-wrapper">
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Email"
+                            className="login-email"
+                        />
+
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="login-password"
+                            required
+                        />
+
+                        <button
+                            onClick={firstclick}
+                            type="submit"
+                            name="login"
+                            className="login-button"
+                        >
+                            {message}
+                        </button>
+                      
+                    </div>
+                      </form>
+                </div>
+            </div>
+       
+    );
 }
 
 export default LoginPage;
