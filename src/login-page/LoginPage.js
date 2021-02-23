835752995dbee3556070246c750f663f658c6e30
Przemall <przemall2@gmail.com>
2021-02-23 16:12:51 +0100

    version 0.2

--- src/login-page/LoginPage.js
+++ src/login-page/LoginPage.js
@@ -2,46 +2,48 @@ import "./loginPage.scss";
 import { useState } from "react";
 
 function LoginPage() {
-    const [message, setMessage] = useState("login");
-    const firstclick = () => {
-        setMessage("login succes!");
-    };
-    return (
-        <div className="main">
-            <div className="leftSide">
-                <div className="image"></div>
-            </div>
-            <div className="rightSide">
-                <form action="/">
-                    <p className="login-title">member login</p>
-                    <div className="input-wrapper">
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="Email"
-                            className="login-email"
-                        />
-
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className="login-password"
-                            required
-                        />
-
-                        <button
-                            onClick={firstclick}
-                            type="submit"
-                            name="login"
-                            className="login-button"
-                        >
-                            {message}
-                        </button>
-                    </div>
-                </form>
-            </div>
+  const [message, setMessage] = useState("login");
+  const firstclick = () => {
+    setMessage("login succes!");
+  };
+  return (
+    <div className="container">
+      <div className="main">
+        <div className="leftSide">
+          <div className="image"></div>
         </div>
-    );
+        <div className="rightSide">
+          <form action="/">
+            <p className="login-title">member login</p>
+            <div className="input-wrapper">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="login-email"
+              />
+
+              <input
+                type="password"
+                placeholder="Password"
+                className="login-password"
+                required
+              />
+
+              <button
+                onClick={firstclick}
+                type="submit"
+                name="login"
+                className="login-button"
+              >
+                {message}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
