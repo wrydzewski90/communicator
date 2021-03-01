62555b1e4872aef6dae8de5fc27552f9bcaee5d3
Przemall <przemall2@gmail.com>
2021-03-01 10:06:48 +0100

    first sketch of MainPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import LoginPage from "./login-page/LoginPage.js";
+import MainPage from "./main-page/MainPage.js";
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <MainPage />
     </div>
   );
 }
---
+++ src/main-page/MainPage.js
@@ -0,0 +1,16 @@
+import "./MainPage.scss";
+
+function MainPage() {
+  return (
+    <div className="container">
+      <div className="leftSide"></div>
+      <div className="rightSide">
+        <div className="header"></div>
+        <div className="messagesField"></div>
+        <div className="typeField"></div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
