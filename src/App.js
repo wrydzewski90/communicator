75c424278113633717c6de1900708131636ebf6e
Przemall <przemall2@gmail.com>
2021-03-05 12:32:17 +0100

    connecting MainPage with app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import LoginPage from "./login-page/LoginPage.js";
 import MainPage from "./main-page/MainPage.js";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <MainPage />
-    </div>
-  );
+  const [userData, setUserData] = useState(null);
+  if (userData) {
+    return (
+      <div>
+        <MainPage />
+      </div>
+    );
+  } else {
+    return <LoginPage onSucces={setUserData} />;
+  }
 }
 
 export default App;
